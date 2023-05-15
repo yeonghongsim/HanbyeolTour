68d1f22c5ba29f78788a63bc827b72901e2462af
hyuuuuuun <jhp6150@gmail.com>
2023-05-15 16:30:12 +0900

    수정

---
+++ src/main/java/com/project/team/member/controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.project.team.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+	//회원가입 페이지로 이동 
+		@GetMapping("/join")
+		public String joinForm() {
+			
+			return "content/member/join";
+		}
+	
+	
+	
+	
+}
