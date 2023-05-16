40d2e0436d909e77222f0e9e9becf46ba14f4328
yeonghongsim <a93200551@gmail.com>
2023-05-16 15:14:38 +0900

    work by hong

---
+++ src/main/java/com/project/team/board/vo/TypeRequestVO.java
@@ -0,0 +1,13 @@
+package com.project.team.board.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TypeRequestVO {
+	private int typeReqeustCode;
+	private String typeRequestName;
+}
