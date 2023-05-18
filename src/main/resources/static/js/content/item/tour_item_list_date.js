f694d59d0d876434df322c342bf626b2e13310c3
DJLastP <DJLastP12345@gmail.com>
2023-05-18 17:29:26 +0900

    일자별 상세검색페이지

--- src/main/resources/static/js/content/item/tour_item_list_date.js
+++ src/main/resources/static/js/content/item/tour_item_list_date.js
@@ -1,9 +1,19 @@
-getSearchByDateTable();
+init();
 
 
-function getSearchByDateTable(){
+//페이지가 로딩될때 실행될 자바스크립트
+function init (){
+	//달력테이블 그리기
+	getSearchByDateTable();
 	
+	setArrDepDate();
 	
+}
+
+
+
+//일자별상품조회 달력
+function getSearchByDateTable(month){
 	
 	//년 월 정보가 세팅될 태그
 	const yearSpanTag = document.querySelector('#yearSpan');
@@ -15,45 +25,57 @@ function getSearchByDateTable(){
 	//테이블 지우기
 	searchByDateCal.replaceChildren();
 	
-	
-	
 	//오늘 년월일정보 
 	date = new Date();
 
 	//오늘 날짜 요일정보 세팅
-	const nowYear = date.getFullYear();
+	let nowYear = date.getFullYear();
 	let nowMonth = date.getMonth() + 1;
 	const nowDate = date.getDate() + 1;
 	const nowDay = date.getDay();
 	const dayName = ['일', '월', '화', '수', '목','금','토'];
+	let monthSpanTagVal = nowMonth;
+	
+	//이번달 다음달 조회
+	if (month != null && month == 'prev') {
+		month = -1;
+		nowYear = parseInt(yearSpanTag.textContent);
+	}
+	else if (month != null && month == 'next') {
+		month = 1;
+		nowYear = parseInt(yearSpanTag.textContent);
+	}
 	
 	
-	//다음달으로 넘기기
+	if(monthSpanTag.textContent != null && !monthSpanTag.textContent == ''){
+		
+		nowMonth = parseInt(monthSpanTag.textContent) + month;
 	
-	
-	
-	
-	//
-	if(monthSpanTag.textContent == null || monthSpanTag.textContent == ''){
-		monthSpanTag.textContent = nowMonth;
 	}
 	else{
-		
-		monthSpanTag.textContent = parseInt(monthSpanTag.textContent) + 1;
-		nowMonth = parseInt(monthSpanTag.textContent);
-		console.log(nowMonth);
+		monthSpanTag.textContent = monthSpanTagVal;
 		
 	}
 	
 	//이번달 마지막 날짜 조회
-	
-	const getLastDate = new Date(nowYear, nowMonth, 0);
-	const lastDate = getLastDate.getDate();
+	const getDate = new Date(nowYear, nowMonth, 0);
+	const lastDate = getDate.getDate();
 	let drawCalTable ='';
 	
 	//html에 년도 월 정보 출력
-	yearSpanTag.textContent = nowYear + '\.';
-	console.log(monthSpanTag.textContent);
+	
+	if(nowMonth > 12){
+		nowYear = nowYear + 1;
+		nowMonth = 1;
+	}
+	else if(nowMonth < 1){
+		nowYear = nowYear - 1;
+		nowMonth = 12;
+		
+	}
+	
+	yearSpanTag.textContent = nowYear;
+	monthSpanTag.textContent = nowMonth;
 	
 	//요일 설정
 	let getDays = [];
@@ -64,7 +86,7 @@ function getSearchByDateTable(){
 	}
 	
 	
-	//요일	
+	//요일 테이블
 	drawCalTable += `<tr>`
 	for(let i = 0; i < lastDate; i++){
 		
@@ -82,7 +104,7 @@ function getSearchByDateTable(){
 	drawCalTable += `</tr>`	
 	
 	
-	//날짜
+	//날짜테이블
 	drawCalTable += `<tr>`
 	
 	for(let i = 0; i < lastDate; i++){
@@ -93,5 +115,65 @@ function getSearchByDateTable(){
 	//테이블 html에 넣기
 	searchByDateCal.insertAdjacentHTML('afterbegin', drawCalTable);
 	 
+}
+
+
+//달력의 날짜 클릭시 검색결과
+function getSearchByDateAJAX(){
+		
+	const areaName = document.querySelector('#areaName').value; 
+	console.log(areaName.value);
+		
+	$.ajax({
+		url: '/item/tourItemListDateAJAX', //요청경로
+		type: 'post',
+		//contentType : 'application/json; charset=UTF-8',
+		//contentType : "application/x-www-form-urlencoded; charset=UTF-8",
+		//async : true,
+		data: {'areaName': areaName}, 
+		success: function(result) {
+			console.log(result);
+		},
+		error: function() {
+			alert('실패');
+		}
+	});
+	
+}
+
+
+function setArrDepDate(){
+	
+	
+	let nowYear = date.getFullYear();
+	let nowMonth = date.getMonth() + 1;
+	let nowDate = date.getDate();
+	const endDate = document.querySelector('#endDate').value;
+	//출발일
+	const depDate = '<td><div>' + nowYear + '년' + nowMonth + '월' + nowDate + '일</div>';
+	
+	
+	
+	//도착일
+	let arrDate = nowDate + endDate;
+	
+	const getDate = new Date(nowYear, nowMonth, 0);
+	const lastDate = getDate.getDate();
+	
+	if(arrDate > lastDate){
+		arrDate = 0 + endDate;
+		nowMonth += 1;
+		
+		if(nowMonth > 12){
+			nowMonth = 1;
+			nowYear = nowYear + 1;
+		}
+	}
+	arrDate = '<div>' + nowYear + '년' + nowMonth + '월' + nowDate + '일' + '</div></td>';
+	
+	
+	document.querySelector('.searchResultByDate').insertAdjacentHTML('afterbegin', depDate + arrDate);
+	
+	
 }
 
