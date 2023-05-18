9c6c65765cae51f0ec4b9e9076ea695d87cea20a
byeol93 <byuuuuul@gmail.com>
2023-05-18 17:25:48 +0900

    saleManage

--- src/main/java/com/project/team/util/ImgPath.java
+++ src/main/java/com/project/team/util/ImgPath.java
@@ -3,9 +3,3 @@ package com.project.team.util;
 public class ImgPath {
 	//이미지경로 
 	public static final String UPLOAD_PATH = UploadPath.UPLOAD_PATH;
-<<<<<<< HEAD
-
-}
-=======
-}
->>>>>>> 46d7a41addbb380ff5cee65d7b2724fc5137af26
