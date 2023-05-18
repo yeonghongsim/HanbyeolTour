87fdfac8cb89a9ac982950f5fd1e74f62b77211b
byeol93 <byuuuuul@gmail.com>
2023-05-18 17:26:49 +0900

    충돌 수정

--- src/main/java/com/project/team/util/ConstVarialbe.java
+++ src/main/java/com/project/team/util/ConstVarialbe.java
@@ -1,11 +1,10 @@
 package com.project.team.util;
 
 public class ConstVarialbe {
-<<<<<<< HEAD
-=======
+
 	// 첨부 파일 경로
 	public static final String UPLOAD_PATH = "/Users/jisooyoon/Documents/teamWorkspace/HanbyeolTour/src/main/resources/static/img/item/itemImg";
->>>>>>> 46d7a41addbb380ff5cee65d7b2724fc5137af26
+
 	
 			
 }
