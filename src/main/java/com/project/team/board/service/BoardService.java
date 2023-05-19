bdc00254a8538d0be50483ef06e9a362050e7b32
yeonghongsim <a93200551@gmail.com>
2023-05-19 14:40:01 +0900

    hong

--- src/main/java/com/project/team/board/service/BoardService.java
+++ src/main/java/com/project/team/board/service/BoardService.java
@@ -2,10 +2,15 @@ package com.project.team.board.service;
 
 import java.util.List;
 
+import com.project.team.board.vo.BoardSideMenuVO;
 import com.project.team.board.vo.TypeRequestVO;
 
 public interface BoardService {
-	
+	// 문의 유형 목록 조회
 	List<TypeRequestVO> getTypeRequestList();
+	
+	// 보드 사이드 메뉴 목록 조회
+	List<BoardSideMenuVO> getBoardSideMenuList();
+	
 	
 }
