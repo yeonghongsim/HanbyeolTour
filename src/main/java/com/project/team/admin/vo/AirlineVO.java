ecd6243814cd2a6e7e240615cf7a32b51bcc5d70
DJLastP <DJLastP12345@gmail.com>
2023-05-30 17:17:50 +0900

    항공사관리페이지완료 투어상품관리진행중

---
+++ src/main/java/com/project/team/admin/vo/AirlineVO.java
@@ -0,0 +1,15 @@
+package com.project.team.admin.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class AirlineVO {
+    private String hbtAirlineCode;
+    private String hbtAirlineName;
+    private String hbtAirlineIcon;
+    private String isUse;
+}
