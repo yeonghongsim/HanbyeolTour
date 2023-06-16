7348b88ec336edc6f3eeeeb99df0d9a71afe4551
hyuuuuuun <jhp6150@gmail.com>
2023-06-16 13:45:03 +0900

    6/16 오전 취합

--- src/main/java/com/project/team/member/service/MemberServiceImpl.java
+++ src/main/java/com/project/team/member/service/MemberServiceImpl.java
@@ -174,7 +174,11 @@ public class MemberServiceImpl implements MemberService{
 		return sqlSession.selectOne("memberMapper.getItemDetailForQna", itemCode);
 	}
 	
-	
+	// 마이페이지 메인 페이지 - 리뷰 리스트 조회 
+	@Override
+	public List<MemberReviewVO> getMyPageReviewList(String memCode) {
+		return sqlSession.selectList("memberMapper.getMyPageReviewList", memCode);
+	}
 	
 	
 	
@@ -229,6 +233,8 @@ public class MemberServiceImpl implements MemberService{
 	public List<BuyVO> getNeedReviewList(String memCode) {
 		return sqlSession.selectList("memberMapper.getNeedReviewList", memCode);
 	}
+
+	
 
 	
 	
